c0b7765715dc98b306de5b8b063b9a8094545308
shimak <shimak.a@aeturnum.com>
2017-07-20 10:47:29 +0530

    Hide Keyboard

--- BlueLibrary/AddAlbumViewController.m
+++ BlueLibrary/AddAlbumViewController.m
@@ -21,28 +21,35 @@
 @implementation AddAlbumViewController
 - (IBAction)hideKeyboard:(id)sender {
     NSLog(@"Touch Gesture");
+    UITextField *someTextField = (UITextField *)[_addAlbumForm viewWithTag:5];
+    [someTextField resignFirstResponder];
 }
 
 - (IBAction)addBtn:(id)sender {
-    UIDatePicker *date = [_addAlbumForm viewWithTag:4];
-    NSDateComponents *components = [[NSCalendar currentCalendar] components:NSCalendarUnitDay | NSCalendarUnitMonth | NSCalendarUnitYear fromDate:date.date];
-    
+    UIDatePicker *datePicker = [_addAlbumForm viewWithTag:4];
+    NSDateComponents *components = [[NSCalendar currentCalendar]
+            components:NSCalendarUnitDay | NSCalendarUnitMonth | NSCalendarUnitYear
+              fromDate:datePicker.date];
+
     Album *album = [[Album alloc] initWithTitle:[(UITextField *) [_addAlbumForm viewWithTag:2] text]
                                          artist:[(UITextField *) [_addAlbumForm viewWithTag:1] text]
                                        coverUrl:[(UITextField *) [_addAlbumForm viewWithTag:5] text]
                                            year:[[NSString alloc] initWithFormat:@"%d", [components year]]];
-    
+
     [[LibraryAPI sharedInstance] addAlbumAtLast:album];
-    
-    NSLog(@"Album%@",album);
-    
+
+    NSLog(@"Album%@", album);
+
     ViewController *addAlbum = [ViewController new];
     [self presentViewController:addAlbum animated:YES completion:nil];
 }
 
 - (void)viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
-    UIBarButtonItem *backButton = [[UIBarButtonItem alloc] initWithTitle:@"Back" style:UIBarButtonItemStyleBordered target:self action:@selector(Back)];
+    UIBarButtonItem *backButton = [[UIBarButtonItem alloc] initWithTitle:@"Back"
+                                                                   style:UIBarButtonItemStyleBordered
+                                                                  target:self
+                                                                  action:@selector(Back)];
     _navBar.topItem.leftBarButtonItem = backButton;
 }
 
@@ -57,7 +64,7 @@
     [url setDelegate:self];
 }
 
--(BOOL)textFieldShouldReturn:(UITextField *)textField{
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
     [textField resignFirstResponder];
     return YES;
 }
